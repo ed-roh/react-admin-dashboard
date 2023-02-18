9900ceb919ee208a94c594dbb61dab08501562d3
vcpersike <vcpersike@gmail.com>
2023-02-18 05:54:44 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
 
+
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
