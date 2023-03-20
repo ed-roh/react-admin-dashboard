3933b9af8304a0c98c99213b690353cc928d46f8
Ubaidullah <researchfigma@gmail.com>
2023-03-20 19:26:29 +0500

    add db.json file

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -1,21 +1,21 @@
-import { tokens } from "../theme";
-
-export const mockDataTeam = [
-  {
-    id: 1,
-    name: "Jon Snow",
-    email: "jonsnow@gmail.com",
-    age: 35,
-    phone: "(665)121-5454",
-    access: "admin",
-  },
-  {
-    id: 2,
-    name: "Cersei Lannister",
-    email: "cerseilannister@gmail.com",
-    age: 42,
-    phone: "(421)314-2288",
-    access: "manager",
+import { tokens } from "../theme"; 
+ 
+export const mockDataTeam = [ 
+  { 
+    id: 1, 
+    name: "Jon Snow", 
+    email: "jonsnow@gmail.com", 
+    age: 35, 
+    phone: "(665)121-5454", 
+    access: "admin", 
+  }, 
+  { 
+    id: 2, 
+    name: "Cersei Lannister", 
+    email: "cerseilannister@gmail.com", 
+    age: 42,  
+    phone: "(421)314-2288", 
+    access: "manager", 
   },
   {
     id: 3,
