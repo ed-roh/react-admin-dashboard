a9bf92934505305588d9c9633224b846c83374e4
Hichem <hichembouzourine.uni@gmail.com>
2023-04-29 18:28:41 +0100

    Feat(axios) - configure Authorization header

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,5 +1,14 @@
 import axios from "axios";
 
-export default axios.create({
+const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
+
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  return config;
+});
+
+export default instance;
