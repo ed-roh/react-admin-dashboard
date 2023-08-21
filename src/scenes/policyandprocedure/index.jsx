99ce12e27cb4261292a40c5fe532627ff30d4b1b
Eric Hester <eric.hester@umbrella.tech>
2023-08-20 21:55:46 -0400

    fixed policy formatting

--- src/scenes/policyandprocedure/index.jsx
+++ src/scenes/policyandprocedure/index.jsx
@@ -9,7 +9,6 @@ import {
   EditOutlined,
   PublishedWithChangesOutlined,
   PreviewOutlined,
-  SavedSearchTwoTone,
   CloseRounded,
 } from "@mui/icons-material";
 import React, { useState, useEffect, Component } from "react";
@@ -17,6 +16,10 @@ import { convertToRaw, convertFromRaw, EditorState } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import "./editor.css";
+import SimpleBackDrop from "../../components/SimpleBackDrop";
+
+let rows = [];
+let realrows = [];
 
 export default function PolicyandProcedure() {
   const [documents, setDocuments] = useState([]);
@@ -24,6 +27,7 @@ export default function PolicyandProcedure() {
   const supabase = useSupabaseClient();
   const [open, setOpen] = useState(false);
   const [name, setName] = useState();
+  const [isLoading, setIsLoading] = useState(true);
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   );
@@ -45,7 +49,6 @@ export default function PolicyandProcedure() {
       })
       .catch((err) => {
         console.log("no data");
-
         setEditorState(EditorState.createEmpty());
         console.log(err);
       });
@@ -59,7 +62,7 @@ export default function PolicyandProcedure() {
   }, [editorState]);
 
   function saveContent(name, raw) {
-    console.log("saving", raw);
+    console.log("saving", name, raw);
 
     supabase
       .from("policies")
@@ -80,26 +83,55 @@ export default function PolicyandProcedure() {
 
   const renderButton = (params) => {
     return (
-      <strong>
-        <IconButton
-          variant="contained"
-          color="secondary"
-          size="small"
-          style={{ marginLeft: 16 }}
-        >
-          {params.field === "editbutton" ? (
-            //on click se tname to params.name and open editor
-            <IconButton onClick={() => openEditor(params.row.name)}>
-              <EditOutlined />
-            </IconButton>
-          ) : null}
-          {params.field === "loadbutton" ? <InputOutlined /> : null}
-          {params.field === "approvebutton" ? (
+      <>
+        {params.field === "editbutton" ? (
+          <IconButton
+            variant="contained"
+            color="secondary"
+            size="small"
+            style={{ marginLeft: 16 }}
+            onClick={() => openEditor(params.row.name)}
+          >
+            <EditOutlined />
+          </IconButton>
+        ) : null}
+
+        {params.field === "loadbutton" ? (
+          <IconButton
+            variant="contained"
+            color="secondary"
+            size="small"
+            style={{ marginLeft: 16 }}
+            onClick={() => loadTemplate(params.row.name)}
+          >
+            <InputOutlined />
+          </IconButton>
+        ) : null}
+
+        {params.field === "approvebutton" ? (
+          <IconButton
+            variant="contained"
+            color="secondary"
+            size="small"
+            style={{ marginLeft: 16 }}
+            onClick={() => markApprove(params.row.name)}
+          >
             <PublishedWithChangesOutlined />
-          ) : null}
-          {params.field === "reviewbutton" ? <PreviewOutlined /> : null}
-        </IconButton>
-      </strong>
+          </IconButton>
+        ) : null}
+
+        {params.field === "reviewbutton" ? (
+          <IconButton
+            variant="contained"
+            color="secondary"
+            size="small"
+            style={{ marginLeft: 16 }}
+            onClick={() => markReview(params.row.name)}
+          >
+            <PreviewOutlined />
+          </IconButton>
+        ) : null}
+      </>
     );
   };
 
@@ -119,246 +151,79 @@ export default function PolicyandProcedure() {
     {
       field: "editbutton",
       headerName: "Edit",
-      width: 50,
+      width: 40,
       renderCell: renderButton,
     },
     {
       field: "loadbutton",
       headerName: "Load",
-      width: 100,
+      width: 50,
       renderCell: renderButton,
     },
-    { field: "name", headerName: "Document Name", width: 300 },
-    { field: "status", headerName: "Status", width: 100 },
-    { field: "owner", headerName: "Owner", width: 150 },
-    { field: "created_by", headerName: "Created By", width: 150 },
-    { field: "created_at", headerName: "Created", width: 150 },
-    { field: "modified_by", headerName: "Modified By", width: 150 },
-    { field: "last_modified_at", headerName: "Modified", width: 150 },
-    { field: "next_review", headerName: "Next Review", width: 150 },
     {
       field: "reviewbutton",
       headerName: "Review",
-      width: 65,
+      width: 50,
       renderCell: renderButton,
     },
     {
       field: "approvebutton",
       headerName: "Approve",
-      width: 75,
+      width: 65,
       renderCell: renderButton,
     },
+    { field: "name", headerName: "Document Name", width: 250 },
+    { field: "status", headerName: "Status", width: 100 },
+    { field: "owner", headerName: "Owner", width: 100 },
+    { field: "created_at", headerName: "Created", width: 120 },
+    { field: "last_modified_at", headerName: "Last Modified", width: 120 },
+    { field: "modified_by", headerName: "Modified By", width: 175 },
+    { field: "next_review", headerName: "Next Review", width: 120 },
+
   ];
 
-  const rows = [
-    {
-      id: 1,
-      name: "Acceptable Use Policy",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 2,
-      name: "Account and Access Management Policy",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 3,
-      name: "Asset Inventory Procedure",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 4,
-      name: "Change Control Policy and Procedures",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 5,
-      name: "Information/Data Security Policy",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 6,
-      name: "Disaster Recovery / Business Continuity Plan",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 7,
-      name: "HR Onboarding and Offboarding Policies",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 8,
-      name: "Incident Response Plan",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 9,
-      name: "Internal Audit / Continuous Improvement Plan",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 10,
-      name: "Server Management Policy",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 11,
-      name: "Workstation Management Policy",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 12,
-      name: "Mobile Device Management Policy",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 13,
-      name: "Remote Work and BYOD Policy",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 14,
-      name: "Physical Access Control Policy",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 15,
-      name: "Risk Management Policy",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 16,
-      name: "Software Update Policy",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 17,
-      name: "Network Security Policy",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 18,
-      name: "Identity & Credential Management Policy",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 19,
-      name: "Security Awareness and Training Policy",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 20,
-      name: "Financial Controls Policy",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 21,
-      name: "Child Safety Policy",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 22,
-      name: "Volunteer Onboarding Policy",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-    {
-      id: 23,
-      name: "Safeguarding Member Information Policy",
-      status: "Missing",
-      created_by: "",
-      created_at: "",
-      modified_by: "",
-      last_modified_at: "",
-    },
-  ];
+  supabase
+    .from("policies")
+    .select("*")
+    .eq("customer_id", user.id)
+    .then((data) => {
+      realrows = data.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  supabase
+    .from("policy_templates")
+    .select("*")
+    .then((data) => {
+      rows = data.data.map((row, index) => ({
+        id: index,
+        name: row.name,
+        status: realrows.find((r) => r.name === row.name)?.status || "Missing",
+        owner: realrows.find((r) => r.name === row.name)?.owner || "",
+        created_by: realrows.find((r) => r.name === row.name)?.created_by || "",
+        created_at:
+          realrows.find((r) => r.name === row.name)?.created_at.slice(5, 10) +
+            "-" +
+            realrows.find((r) => r.name === row.name)?.created_at.slice(0, 4) ||
+          "",
+        modified_by:
+          realrows.find((r) => r.name === row.name)?.modified_by || "",
+        last_modified_at:
+          realrows
+            .find((r) => r.name === row.name)
+            ?.last_modified_at.slice(5, 10) +
+            "-" +
+            realrows
+              .find((r) => r.name === row.name)
+              ?.last_modified_at.slice(0, 4) || "",
+      }));
+      setIsLoading(false);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
   const ODD_OPACITY = 0.2;
 
@@ -393,6 +258,10 @@ export default function PolicyandProcedure() {
     },
   }));
 
+  if (isLoading) {
+    return <SimpleBackDrop />;
+  }
+
   return (
     <>
       <Box>
@@ -425,7 +294,7 @@ export default function PolicyandProcedure() {
       <Box m="20px">
         <Header title="Policy and Procedure Library" />
       </Box>
-      <Box m="30px 30px" height="75vh">
+      <Box m="30px 30px" height="75vh" width="80vw">
         <Box m="30px 30px"></Box>
         <Box height="75vh">
           <Button
