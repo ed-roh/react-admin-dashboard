66b7056de0109f00fc0fb5605cf38f52ae7fc0fc
Majd Jaber <majd.jaber@hp.com>
2024-01-15 17:39:43 +0200

    app works, with basic functionality of the election table that ws implemented in elections-v1 copied here --CODE still not clean, many extra components

--- src/scenes/invoices/index.jsx
+++ src/scenes/invoices/index.jsx
@@ -1,51 +1,126 @@
+import React, { useState } from "react";
 import { Box, Typography, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
 import { mockDataInvoices } from "../../data/mockData";
 import Header from "../../components/Header";
 
+import MaterialTable from 'material-table'
+import '@mui/material/styles';
+import '@mui/icons-material';
+import {  ThemeProvider } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
+
+import { FirstPage, LastPage, ChevronLeft, ChevronRight, Search, Clear } from '@mui/icons-material';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import Checkbox from '@mui/material/Checkbox';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+
 const Invoices = () => {
   const theme = useTheme();
+  const [tableKey, setTableKey] = useState(0);
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+      textAlign: 'right', // Align text to the right in body cells
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+      textAlign: 'right', // Align text to the right in body cells
+
+    },
+  }));
+
+  const StyledVotedTableCell = styled(TableCell)(({ theme, voted }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+      textAlign: 'right', // Align text to the right in body cells
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+      backgroundColor: voted ? "#90EE90" : "#FF7F7F",
+      textAlign: 'right', // Align text to the right in body cells
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
   const colors = tokens(theme.palette.mode);
   const columns = [
-    { field: "id", headerName: "ID" },
+    // {
+    //   title: "Change",
+    //   field: "edit",
+    //   render: (rowData) => (
+    //     <>
+    //       <Checkbox
+    //         checked={rowData.checkboxStatus}
+    //         onChange={() => handleCheckboxChange(rowData)}
+    //       />
+    //       <button onClick={() => handleApply(rowData, rowData.checkboxStatus)}>
+    //         Apply
+    //       </button>
+    //     </>
+    //   ),
+    //   cellStyle: { textAlign: 'right' },
+    //   headerStyle: { textAlign: 'right' }
+    // },
     {
-      field: "name",
-      headerName: "Name",
-      flex: 1,
-      cellClassName: "name-column--cell",
+      title: "הצביע", field: "voted",
+      cellStyle: { textAlign: 'right', className: 'custom-cell' },
+      headerStyle: { textAlign: 'right', className: 'custom-header' }
     },
     {
-      field: "phone",
-      headerName: "Phone Number",
-      flex: 1,
+      title: "מס קלפי", field: "ballot",
+      cellStyle: { textAlign: 'right', className: 'custom-cell' },
+      headerStyle: { textAlign: 'right', className: 'custom-header' }
     },
     {
-      field: "email",
-      headerName: "Email",
-      flex: 1,
+      title: "מס בית", field: "house_number",
+      cellStyle: { textAlign: 'right' },
+      headerStyle: { textAlign: 'right' }
     },
     {
-      field: "cost",
-      headerName: "Cost",
-      flex: 1,
-      renderCell: (params) => (
-        <Typography color={colors.greenAccent[500]}>
-          ${params.row.cost}
-        </Typography>
-      ),
+      title: "רחוב", field: "street",
+      cellStyle: { textAlign: 'right' },
+      headerStyle: { textAlign: 'right' }
     },
     {
-      field: "date",
-      headerName: "Date",
-      flex: 1,
+      title: "שם אב", field: "father_name",
+      cellStyle: { textAlign: 'right' },
+      headerStyle: { textAlign: 'right' }
     },
+    {
+      title: "שם משפחה", field: "last_name",
+      cellStyle: { textAlign: 'right' },
+      headerStyle: { textAlign: 'right' }
+    },
+    {
+      title: "שם פרטי", field: "first_name",
+      cellStyle: { textAlign: 'right' },
+      headerStyle: { textAlign: 'right' }
+    },
+    {
+      title: "ת.ז", field: "id",
+      cellStyle: { textAlign: 'right' },
+      headerStyle: { textAlign: 'right' }
+    }
   ];
+
 
   return (
     <Box m="20px">
       <Header title="INVOICES" subtitle="List of Invoice Balances" />
-      <Box
+      {/* <Box
         m="40px 0 0 0"
         height="75vh"
         sx={{
@@ -75,8 +150,89 @@ const Invoices = () => {
         }}
       >
         <DataGrid checkboxSelection rows={mockDataInvoices} columns={columns} />
-      </Box>
+      </Box> */}
+      <>
+            <ThemeProvider theme={theme}>
+              <MaterialTable
+                key={tableKey}
+                className="custom-material-table"
+                title="רשימת המצביעים"
+                columns={columns.map((col) => ({
+                  ...col,
+                  render: (rowData) => {
+                    if (col.field === "edit") {
+                      return col.render(rowData);
+                    } else if (col.field === "voted") {
+                      return (
+                        <StyledTableRow>
+                          <StyledVotedTableCell align="right" voted={rowData.voted}>
+                            {rowData.voted ? <>&#x2713;</> : <>&times;</>}
+                          </StyledVotedTableCell>
+                        </StyledTableRow>
+                      );
+                    } else {
+                      return (
+                        <StyledTableRow>
+                          <StyledTableCell align="right">
+                            {rowData[col.field]}
+                          </StyledTableCell>
+                        </StyledTableRow>
+                      );
+                    }
+                  },
+                }))}
+                options={{
+                  debounceInterval: 700,
+                  padding: "dense",
+                  filtering: true,
+                  pageSize: 25,
+                  pageSizeOptions: [],
+                }}
+                data={(query) =>
+                  new Promise((resolve, reject) => {
+                    let url = 'https://elections-bice.vercel.app/v1/elections/data?';
+                    if (query.search) {
+                      url += `q=${query.search}`;
+                    }
+                    if (query.filters.length) {
+                      const filter = query.filters.map((filter) => {
+                        return `${filter.column.field}${filter.operator}${filter.value}`;
+                      });
+                      url += `${filter.join('')}`;
+                    }
+                    url += `page=${query.page + 1}`
+                    if (query.orderBy) {
+                      url += `&sort=${query.orderBy.field}:${query.orderDirection}`;
+                    };
+
+                    fetch(url)
+                      .then((resp) => resp.json())
+                      .then((resp) => {
+                        resolve({
+                          data: resp.results,
+                          page: query.page,
+                          totalCount: 5854,
+                        });
+                      });
+                  })
+                }
+                icons={{
+                  FirstPage: FirstPage,
+                  LastPage: LastPage,
+                  NextPage: ChevronRight,
+                  PreviousPage: ChevronLeft,
+                  Filter: FilterListIcon,
+                  SortArrow: ArrowDownwardIcon,
+                  Search: Search,
+                  Clear: Clear
+                }}
+              />
+            </ThemeProvider>
+            {/* ... existing commented out components */}
+          </>
     </Box>
+
+    
   );
 };
 
